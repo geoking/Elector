234d18f25b7b1aabe7feaec095c37cfd9c475f21
George King <me@gking.uk>
2020-07-01 12:34:09 +0100

    Added initial project

---
+++ XCode/Elector/Shared/ElectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectorApp.swift
+//  Shared
+//
+//  Created by George King on 01/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct ElectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
